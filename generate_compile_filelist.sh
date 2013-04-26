41389f6041d2d29dcf7903d86fc93fcf74aa050b
Anders Rønningen <anr@inne.proxdynamics.com>
2013-04-26 09:00:39 +0200

    Use inotifywait to generate a list of c, h and S files accessed during a build

---
+++ generate_compile_filelist.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Start a watch on your source directory before compiling your code/project.
+# When the compilation is done, hit enter and get a list of the chS files 
+# accessed during the build. I use this to get a cscope.files with only the
+# relevant files for my linux/u-boot builds
+
+WATCHED_DIR=$1
+OUTFILE=$2
+
+echo "Watching directory: $WATCHED_DIR"
+inotifywait -r -m -e access "$WATCHED_DIR" --format "%w%f" > $OUTFILE.tmp &
+INWJOB=$!
+read -p "Press [Enter] key when done watching..."
+kill $INWJOB
+sort -u $OUTFILE.tmp | grep -E '\.[chS]$' > $OUTFILE
+
